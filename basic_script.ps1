b31b9ff3e1159b85cd18ed3843e94945166e19bb
sstanev <102998517+sjstanev@users.noreply.github.com>
2023-10-26 22:34:02 +0300

    Update basic_script.ps1

--- basic_script.ps1
+++ basic_script.ps1
@@ -1,1 +1,8 @@
 Get-Alias cls
+Rename-Computer -NewName "mail"
+shutdowm /r /t 0
+Get-NetAdapter -Physical | ? {$_.Status -eq "Up"}
+
+
+get-aduser -filter * -Properties * | select name,  @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogon)}} | ForEach-Object { if ($_.lastlogon.year -eq "2023" -and $_.name -notlike "HealthMailbox*" ) {$_.name + " <--> " + $_.las
+tlogon} }
