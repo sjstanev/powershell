31baf4e87067a49af01b1b42a395dbe30adb5f5b
sstanev <102998517+sjstanev@users.noreply.github.com>
2024-01-13 20:34:46 +0200

    up

---
+++ set-dnsclinetaddress.ps1
@@ -0,0 +1,3 @@
+﻿Get-DnsClientServerAddress -InterfaceIndex 8 
+Set-DnsClientServerAddress -InterfaceIndex 8 -ServerAddresses ("10.61.0.10", "10.81.0.10")
+Get-DnsClientServerAddress -InterfaceIndex 8
